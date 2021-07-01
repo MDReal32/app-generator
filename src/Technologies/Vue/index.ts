c104421c4a5006cf82eaff08d946b3eef6be00e7
MDReal <veysaliyev00@gmail.com>
2021-07-01 13:22:48 +0400

    Vue Generator WIP

---
+++ src/Technologies/Vue/index.ts
@@ -0,0 +1,4 @@
+import { IConfig } from "../../config";
+import { PackageManager } from "../../PackageManager/PackageManager";
+
+export const Vue = (config: IConfig, packageManager: PackageManager) => {};
