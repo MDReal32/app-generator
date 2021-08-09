f475ba7b46d2583c18dbbe64ed54e7d158dd3b0c
MDReal <veysaliyev00@gmail.com>
2021-08-09 15:50:19 +0400

    Updated

--- src/Technologies/Vue/index.ts
+++
@@ -1,4 +0,0 @@
-import { IConfig } from "../../config";
-import { PackageManager } from "../../PackageManager/PackageManager";
-
-export const Vue = (config: IConfig, packageManager: PackageManager) => {};
