617e3a95cce4126c138d97b0130fca5450e8f1b1
MDReal <veysaliyev00@gmail.com>
2021-08-09 16:01:13 +0400

    Updated Core questions

--- src/config.ts
+++ src/config.ts
@@ -11,157 +11,92 @@ import {
 } from "./typescript/enums";
 import { IConfig } from "./typescript/interfaces";
 
-export enum Frameworks {
-  Express = "expressjs",
-  NestJS = "nestjs"
-}
+export const getConfig = async (root: string): Promise<IConfig> => {
+  const base = await question(Questions.ROOT, root);
+  let baseDir = resolve(root, base);
+  let projectName = await question(Questions.PROJ_NAME, basename(baseDir));
+  const description = await question(Questions.DESCRIPTION, "");
+  const author = await question(Questions.AUTHOR, "");
+  const tech = await listQuestion<keyof typeof Technologies>(
+    Questions.SELECT_TECH,
+    ...Object.keys(Technologies)
+  );
 
-export enum CSSPreProcessors {
-  Plain = "plain",
-  Less = "less",
-  Stylus = "stylus",
-  Compass = "compass",
-  Sass = "sass"
-}
+  if (projectName === ".") {
+    projectName = basename(baseDir);
+    baseDir = dirname(baseDir);
+  }
 
-export enum Engines {
-  NoView = "no-view",
-  EJS = "ejs",
-  HBS = "hbs",
-  HJS = "hjs",
-  Jade = "jade",
-  Twig = "twig",
-  Vash = "vash",
-  Hogan = "hogan",
-  Pug = "pug"
-}
-
-export interface IConfig {
-  technology: Technologies;
-  bundler: Bundlers;
-  framework: Frameworks;
-  ssr: boolean;
-  engine: Engines;
-  cssEngine: CSSPreProcessors;
-  vueVersion: 2 | 3;
-  ts: boolean;
-}
-
-export const getConfig = async () => {
-  const answers: IConfig = await prompt([
-    {
-      name: "technology",
-      message: "Which technology you want use?",
-      type: "list",
-      choices: Object.keys(Technologies)
-    }
-  ]);
-
-  // @ts-ignore
-  answers.technology = Technologies[answers.technology];
+  const config: IConfig = {
+    root: baseDir,
+    name: projectName,
+    description,
+    author,
+    technology: Technologies[tech]
+  };
 
   /// <Express>
-  if (answers.technology === Technologies.Express) {
-    const Framework: IConfig = await prompt({
-      name: "framework",
-      message: "Which Framework do you want use?",
-      type: "list",
-      choices: Object.keys(Frameworks)
-    });
+  if (config.technology === Technologies.Express) {
+    const framework = await listQuestion<keyof typeof Frameworks>(
+      Questions.EXPRESS_FRAMEWORK,
+      ...Object.keys(Frameworks)
+    );
 
-    // @ts-ignore
-    answers.framework = Frameworks[Framework.framework];
+    config.framework = Frameworks[framework];
   }
 
   const isExpress =
-    answers.technology === Technologies.Express && answers.framework === Frameworks.Express;
+    config.technology === Technologies.Express && config.framework === Frameworks.Express;
 
   if (isExpress) {
-    const Engine: IConfig = await prompt({
-      name: "engine",
-      message: "Which Engine do you want to use MVC Model?",
-      type: "list",
-      choices: Object.keys(Engines)
-    });
+    const engine = await listQuestion<keyof typeof Engines>(
+      Questions.EXPRESS_HTML_ENGINE,
+      ...Object.keys(Engines)
+    );
+    const cssPreProcessor = await listQuestion<keyof typeof CSSPreProcessors>(
+      Questions.EXPRESS_CSS_PREPROCESSOR,
+      ...Object.keys(CSSPreProcessors)
+    );
 
-    const CssEngine: IConfig = await prompt({
-      name: "cssEngine",
-      message: "Which CSS Preprocessor do you want to use?",
-      type: "list",
-      choices: Object.keys(CSSPreProcessors)
-    });
-
-    answers.engine = Engine.engine;
-    answers.cssEngine = CssEngine.cssEngine;
+    config.engine = Engines[engine];
+    config.cssPreprocessor = CSSPreProcessors[cssPreProcessor];
   }
   /// </Express>
 
-  // /// <Vue>
-  // if (answers.technology === Technologies.Vue) {
-  //   const VueVersions: IConfig = await prompt({
-  //     name: "vueVersion",
-  //     message: "Which Vue Version do you want?",
-  //     type: "list",
-  //     choices: ["2", "3"]
-  //   });
-  //
-  //   const SSR: IConfig = await prompt({
-  //     name: "ssr",
-  //     message: "Do you want SSR App?",
-  //     type: "confirm"
-  //   });
-  //
-  //   // @ts-ignore
-  //   answers.vueVersion = parseInt(VueVersions.vueVersion);
-  //   answers.ssr = SSR.ssr;
-  //   answers.bundler = Bundlers.Webpack;
-  // }
-  //
-  // if (answers.vueVersion === 3) {
-  //   const userBundler: IConfig = await prompt({
-  //     name: "bundler",
-  //     message: "Which bundler you want use?",
-  //     type: "list",
-  //     choices: Object.keys(Bundlers)
-  //   });
-  //
-  //   // @ts-ignore
-  //   answers.bundler = Bundlers[userBundler.bundler];
-  // }
-  // /// </Vue>
+  /// <Vue>
+  if (config.technology === Technologies.Vue) {
+    config.vueVersion = await listQuestion<2 | 3>(Questions.VUE_VERSION, 2, 3);
+    config.ssr = await confirmQuestion(Questions.SSR_APP);
+    config.bundler = Bundlers.Webpack;
+  }
+
+  if (config.vueVersion === 3) {
+    const bundler = await listQuestion<keyof typeof Bundlers>(
+      Questions.BUNDLER,
+      ...Object.keys(Bundlers)
+    );
+    config.bundler = Bundlers[bundler];
+  }
+  /// </Vue>
 
   /// <React>
-  if (answers.technology === Technologies.React) {
-    const userBundler: IConfig = await prompt({
-      name: "bundler",
-      message: "Which bundler you want use?",
-      type: "list",
-      choices: Object.keys(Bundlers)
-    });
-
-    const SSR: IConfig = await prompt({
-      name: "ssr",
-      message: "Do you want SSR App?",
-      type: "confirm"
-    });
-
-    // @ts-ignore
-    answers.bundler = Bundlers[userBundler.bundler];
-    answers.ssr = SSR.ssr;
+  if (config.technology === Technologies.React) {
+    const bundler = await listQuestion<keyof typeof Bundlers>(
+      Questions.BUNDLER,
+      ...Object.keys(Bundlers)
+    );
+    config.bundler = Bundlers[bundler];
+    config.ssr = await confirmQuestion(Questions.SSR_APP);
   }
   /// </React>
 
   /// <Typescript>
-  if (answers.framework !== Frameworks.Express) {
-    const TypeScript: IConfig = await prompt({
-      name: "ts",
-      message: "Do you want use TypeScript?",
-      type: "confirm"
-    });
-
-    answers.ts = TypeScript.ts;
+  if (config.framework !== Frameworks.Express) {
+    config.typescript = await confirmQuestion(Questions.TYPESCRIPT);
   }
   /// </Typescript>
 
-  return answers;
+  console.log(prompts);
+
+  return config;
 };
