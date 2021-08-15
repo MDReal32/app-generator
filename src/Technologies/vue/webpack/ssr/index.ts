129c20fbebfe8574ffb0e238a79a593e6a9cc356
MDReal <veysaliyev00@gmail.com>
2021-08-15 16:19:10 +0400

    Nuxt Support

--- src/Technologies/vue/webpack/ssr/index.ts
+++ src/Technologies/vue/webpack/ssr/index.ts
@@ -1,7 +1,17 @@
 import { Builder } from "../../../../Builder/Builder";
 import { CommandBuilder } from "../../../../Builder/CommandBuilder";
 import { IConfig } from "../../../../typescript/interfaces";
+import { getPackageManager } from "../../../../PackageManager";
 
 export const get = (config: IConfig): Builder => {
+  const pm = getPackageManager().getPackageManager();
+  const baseCommand = pm === "yarn" ? "yarn" : "npx";
+  const builder = new CommandBuilder(baseCommand);
+
+  if (pm === "yarn") builder.addArgument("create").addArgument("nuxt-app");
+  else builder.addArgument("create-nuxt-app");
+  builder.addArgument(".");
+  // if (config.typescript) builder.addOption("typescript");
+
   return new CommandBuilder("echo").addArgument("WebpackSSR");
 };
