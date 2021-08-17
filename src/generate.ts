baf83445a7486aefd66865ffbf98619bbc8ef45a
MDReal <veysaliyev00@gmail.com>
2021-08-17 17:55:20 +0400

    Generate Error fix

--- src/generate.ts
+++ src/generate.ts
@@ -4,15 +4,15 @@ import { IConfig } from "./typescript/interfaces";
 import { CommandBuilder } from "./Builder/CommandBuilder";
 import { mergePackage } from "./utils/mergePackage";
 
-export const generate = (config: IConfig) => {
+export const generate = async (config: IConfig) => {
   const { technology, framework = "", bundler = "", ssr = false } = config;
   const frameworkBundler = technology === Technologies.Express ? framework : bundler;
   const isSSR = technology === Technologies.Express ? "" : `/${ssr ? "" : "no-"}ssr`;
   const module = `./Technologies/${technology}/${frameworkBundler}${isSSR}`;
   const { get } = require(module);
   const builder: Builder = get(config);
-  builder.log().build(config.root);
+  await builder.log().build(config.root);
   if (builder instanceof CommandBuilder) {
-    mergePackage(config);
+    await mergePackage(config);
   }
 };
